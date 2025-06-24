530aab04899afbfeab42bc35c6a29f9f9dd2eaf4
Naminar <buravko.va@phystech.edu>
2025-06-24 12:17:16 +0300

    priority update logic [always -> generate]

--- priority_update_logic.v
+++ priority_update_logic.v
@@ -1,7 +1,6 @@
-
 module priority_update_logic #(
-    WAYS=16,
-    PRIORITY_BITS=5
+    parameter WAYS = 16,
+    parameter PRIORITY_BITS = 5
 ) (
     input wire is_state_fetch,
     input wire is_state_read,
@@ -11,32 +10,20 @@ module priority_update_logic #(
     input wire [WAYS-1:0] victim_indicator_i,
     input wire [WAYS-1:0] [PRIORITY_BITS-1:0] priority_set,
 
-    output reg [WAYS-1:0] [PRIORITY_BITS-1:0] new_priority_set
+    output wire [WAYS-1:0] [PRIORITY_BITS-1:0] new_priority_set
 );
 
-always @(*) begin
-for (int i = 0; i < WAYS; i++)
-    if (hit_i[i]) begin
-        // CAUTION
-        if (is_state_fetch)
-            new_priority_set[i] = priority_set[i] + 1;
-        else if (is_state_read)
-            new_priority_set[i] = priority_set[i] - 1;
-        // else if (state == WRITE_REQ)
-        //     new_priority_set[i] = {{PRIORITY_BITS-1{1'b0}}, 1'b1};
-        else
-            new_priority_set[i] = priority_set[i];
+generate
+    genvar i;
+    for (i = 0; i < WAYS; i = i + 1) begin : gen_priority_update
+        assign new_priority_set[i] = hit_i[i] ?
+                                        (is_state_fetch ? (priority_set[i] + PRIORITY_BITS'(1)) :
+                                         is_state_read  ? (priority_set[i] - PRIORITY_BITS'(1)) :
+                                                          priority_set[i]) :
+                                     (miss & victim_indicator_i[i] & is_state_fetch) ?
+                                        {{PRIORITY_BITS-1{1'b0}}, 1'b1} :
+                                        priority_set[i];
     end
-    // else if (miss & victim_indicator_i[i] & (state == FETCH_REQ | state == WRITE_REQ)) begin
-    else if (miss & victim_indicator_i[i] & is_state_fetch) begin
-    // else if (victim_indicator_i[i]) begin
-        new_priority_set[i] = {{PRIORITY_BITS-1{1'b0}}, 1'b1};
-        // if (state == WRITE_REQ & miss)
-        //     new_priority_set[i] = {PRIORITY_BITS{~victim_indicator_i[i]}};
-        // else
-            // new_priority_set[i] = priority_set[i];
-    end else
-        new_priority_set[i] = priority_set[i];
-end
+endgenerate
 
 endmodule
